0bda2ed8f7b47a423f73112bafef2a96c93d5ff1
Nam Ho <nam_ho92@hotmail.com>
2014-07-03 00:46:24 +1000

    just some user input stuff

--- pi-generator.py
+++ pi-generator.py
@@ -5,16 +5,20 @@
 #obtain user input for number of digits
 def get_digits():
     digits = int(input("How many digits of pi do you want? "))
-	while (digits <= 0) or (digits > 10000):
-	    if digits <= 0:
-		    digits = int(input("Please enter a non-negative number. "))
-		else:
-		    digits = int(input("Please enter a number no greater than 10000. "))
 	
-	return digits
+    while (digits <= 0) or (digits > 10000):
+        if digits <= 0:
+    	    digits = int(input("Please enter a non-negative number. "))
+    	else:
+    	    digits = int(input("Please enter a number no greater than 10000. "))
+    
+    return digits
 
+
+#not sure what the purpose of main() is
 def main():
-    return get_digits()
+    print "You want %r digits." % get_digits()
 
+#dunno what this is either
 if __name__ == "__main__":
     main()
