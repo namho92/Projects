0590d70b49202d463e0d92daf7d9889b1ac4824f
Nam Ho <nam_ho92@hotmail.com>
2014-07-02 21:48:57 +1000

    implementing the user input bit

--- pi-generator.py
+++ pi-generator.py
@@ -1,2 +1,20 @@
-print 'Hello World'
-print 'OK'
+#def pi_term(n):
+#    "Returns the nth term in the infinite expansion of pi."
+#	return 4.0 * (-1)^{n+1} / (2.0*n-1)
+
+#obtain user input for number of digits
+def get_digits():
+    digits = int(input("How many digits of pi do you want? "))
+	while (digits <= 0) or (digits > 10000):
+	    if digits <= 0:
+		    digits = int(input("Please enter a non-negative number. "))
+		else:
+		    digits = int(input("Please enter a number no greater than 10000. "))
+	
+	return digits
+
+def main():
+    return get_digits()
+
+if __name__ == "__main__":
+    main()
